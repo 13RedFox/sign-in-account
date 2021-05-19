3bcc71e73901a8eb3b287a75ae8386a5eb645d3d
13RedFox <sergei181989@gmail.com>
2021-05-19 15:52:00 +0300

    fix: fix slider & hover links

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # [Sign in](https://13redfox.github.io/sign-in-account/dist/)
+**Adaptive:**
+`phone = 320`,`tablet = 768`,`desktop = 1440`
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const swiper = new Swiper('.swiper-container', {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	loop: true,
+	autoplay: true,
+	// navigation: {
+	// 	nextEl: '.arrow-right',
+	// 	prevEl: '.arrow-left',
+	// },
+	pagination: {
+		el: '.swiper-pagination',
+	},
+});
