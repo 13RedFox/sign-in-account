b2e882b73e3e287d5936ed61697c1fa7c254ee3e
13RedFox <sergei181989@gmail.com>
2021-05-19 19:44:44 +0300

    fix: form validate

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,52 @@
 'use strict';
 
-const swiper = new Swiper('.swiper-container', {
-	slidesPerView: 1,
-	spaceBetween: 30,
-	loop: true,
-	autoplay: true,
-	// navigation: {
-	// 	nextEl: '.arrow-right',
-	// 	prevEl: '.arrow-left',
-	// },
-	pagination: {
-		el: '.swiper-pagination',
-	},
+document.addEventListener('DOMContentLoaded', function () {
+
+	// ! Slider
+
+	const swiper = new Swiper('.swiper-container', {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		loop: true,
+		autoplay: true,
+		pagination: {
+			el: '.swiper-pagination',
+		},
+	});
+
+	// ! Form
+
+	const form = document.querySelector('.form'),
+				email = form.querySelector('.form__email-input'),
+				password = form.querySelector('.form__password-input'),
+				error = form.querySelectorAll('.error span'),
+				btn = form.querySelector('.form__btn');
+
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		if (!email.value) {
+			email.classList.add('error');
+			error[0].style.display = 'block';
+		} else {
+			email.classList.remove('error');
+			error[0].style.display = 'none';
+			email.classList.add('valid');
+		}
+
+		if (!password.value) {
+			password.classList.add('error');
+			error[1].style.display = 'block';
+		} else {
+			password.classList.remove('error');
+			error[1].style.display = 'none';
+			password.classList.add('valid');
+		}
+
+		if (!email.value && !password.value) {
+			btn.classList.add('disabled')
+		} else {
+			btn.classList.remove('disabled')
+		}
+	});
 });
