a989d35b068631681aba0bdd2e2e398b4aa906e1
13RedFox <sergei181989@gmail.com>
2021-05-20 14:35:09 +0300

    fix: fixed form validate

--- src/js/main.js
+++ src/js/main.js
@@ -16,37 +16,76 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	// ! Form
 
+	const req = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	const form = document.querySelector('.form'),
-				email = form.querySelector('.form__email-input'),
-				password = form.querySelector('.form__password-input'),
-				error = form.querySelectorAll('.error span'),
+				email = form.querySelector('input[name="email"]'),
+				password = form.querySelector('input[name="password"]'),
 				btn = form.querySelector('.form__btn');
+
+	let validEmail = false,
+			validPassword = false;
+
+	function validateEmail() {
+		if (email.value.match(req)) {
+			email.previousElementSibling.classList.add('hidden');
+			email.classList.remove('error');
+			email.classList.add('valid');
+			validEmail = true;
+		} else {
+			email.previousElementSibling.classList.remove('hidden');
+			email.classList.add('error');
+			email.classList.remove('valid');
+			validEmail = false;
+		}
+	}
+
+	function validatePassword() {
+		if (password.value === '' || password.value.length < 6) {
+			password.previousElementSibling.classList.remove('hidden');
+			password.classList.add('error');
+			password.classList.remove('valid');
+			validPassword = false;
+		} else {
+			password.previousElementSibling.classList.add('hidden');
+			password.classList.remove('error');
+			password.classList.add('valid');
+			validPassword = true;
+		}
+	}
+
+	function validateBtn() {
+		if (validEmail === true && validPassword === true) {
+			btn.disabled = false;
+			btn.classList.remove('disabled')
+		}
+		else {
+			btn.disabled = true;
+			btn.classList.add('disabled')
+		}
+	}
 
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
 
-		if (!email.value) {
-			email.classList.add('error');
-			error[0].style.display = 'block';
-		} else {
-			email.classList.remove('error');
-			error[0].style.display = 'none';
-			email.classList.add('valid');
-		}
-
-		if (!password.value) {
-			password.classList.add('error');
-			error[1].style.display = 'block';
-		} else {
-			password.classList.remove('error');
-			error[1].style.display = 'none';
-			password.classList.add('valid');
-		}
-
-		if (!email.value && !password.value) {
-			btn.classList.add('disabled')
-		} else {
-			btn.classList.remove('disabled')
-		}
+		resetForm();
+		console.log('Check');
 	});
+
+	email.addEventListener('input', () => {
+		validateEmail();
+		validateBtn();
+	});
+
+	password.addEventListener('input', () => {
+		validatePassword();
+		validateBtn();
+	});
+
+	function resetForm() {
+		form.reset();
+		email.classList.remove('valid');
+		password.classList.remove('valid');
+		btn.disabled = true;
+		btn.classList.add('disabled');
+	}
 });
